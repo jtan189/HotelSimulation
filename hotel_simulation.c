baf3d65306366535ab88581ebafe3dee16b2774e
Josh Tan <jtan189@gmail.com>
2013-11-16 16:37:00 -0600

    Added initial program. Modified .gitignore for emacs temp files.

---
+++ hotel_simulation.c
@@ -0,0 +1,15 @@
+/*
+  Josh Tan
+  CSCI 474
+  Project 2: Hotel Simulation
+  12/2/13
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[])
+{
+    printf("Implement me.\n");
+}
